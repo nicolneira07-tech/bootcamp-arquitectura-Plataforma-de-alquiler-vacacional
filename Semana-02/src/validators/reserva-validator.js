6ff967470173ae6d893c06b54ce5805af27703a3
nicolneira07-tech <nicolneira07@gmail.com>
2026-03-10 18:13:38 -0500

    Update reserva-validator.js

--- Semana-02/src/validators/reserva-validator.js
+++ Semana-02/src/validators/reserva-validator.js
@@ -1,1 +1,14 @@
 
+class ReservaValidator {
+  validate(reserva) {
+    if (!reserva.huesped) {
+      throw new Error("El huésped es obligatorio");
+    }
+
+    if (new Date(reserva.fechaInicio) >= new Date(reserva.fechaFin)) {
+      throw new Error("La fecha de inicio debe ser menor a la fecha de fin");
+    }
+  }
+}
+
+module.exports = ReservaValidator;
