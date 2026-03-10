ba8f07a16af9d12229ef89a15d272c3ed34e8301
nicolneira07-tech <nicolneira07@gmail.com>
2026-03-10 18:15:01 -0500

    Update index.js

--- Semana-02/src/index.js
+++ Semana-02/src/index.js
@@ -1,1 +1,21 @@
 
+const Reserva = require("./domain/entities/reserva");
+const MemoryRepository = require("./repositories/memory-repository");
+const ReservaValidator = require("./validators/reserva-validator");
+const ReservaService = require("./services/reserva-service");
+
+const repository = new MemoryRepository();
+const validator = new ReservaValidator();
+const service = new ReservaService(repository, validator);
+
+const nuevaReserva = new Reserva(
+  1,
+  101,
+  "2026-07-01",
+  "2026-07-05",
+  "Nicol Neira"
+);
+
+service.crearReserva(nuevaReserva);
+
+console.log(service.listarReservas());
