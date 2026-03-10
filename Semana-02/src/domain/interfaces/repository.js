89cd8fc185ef18375b2688758fee020a4107d0f9
nicolneira07-tech <nicolneira07@gmail.com>
2026-03-10 18:12:09 -0500

    Update repository.js

--- Semana-02/src/domain/interfaces/repository.js
+++ Semana-02/src/domain/interfaces/repository.js
@@ -1,1 +1,12 @@
 
+class Repository {
+  save(data) {
+    throw new Error("Método save() debe ser implementado");
+  }
+
+  findAll() {
+    throw new Error("Método findAll() debe ser implementado");
+  }
+}
+
+module.exports = Repository;
