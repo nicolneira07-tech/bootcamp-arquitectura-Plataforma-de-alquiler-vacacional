6afb3bc5a9a7ca389ef39af6348cc1a9c52e529f
nicolneira07-tech <nicolneira07@gmail.com>
2026-03-10 18:09:09 -0500

    Update propiedad.js

--- Semana-02/src/domain/entities/propiedad.js
+++ Semana-02/src/domain/entities/propiedad.js
@@ -1,1 +1,12 @@
+``` JavaScript
 
+class Propiedad {
+  constructor(id, nombre, ubicacion, precioPorNoche) {
+    this.id = id;
+    this.nombre = nombre;
+    this.ubicacion = ubicacion;
+    this.precioPorNoche = precioPorNoche;
+  }
+}
+
+module.exports = Propiedad;
