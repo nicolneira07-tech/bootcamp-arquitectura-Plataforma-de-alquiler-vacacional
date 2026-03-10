7d442776011785c0537cf81e34ad009654f7183e
nicolneira07-tech <nicolneira07@gmail.com>
2026-03-10 18:13:00 -0500

    Update memory-repository.js

--- Semana-02/src/repositories/memory-repository.js
+++ Semana-02/src/repositories/memory-repository.js
@@ -1,1 +1,20 @@
 
+const Repository = require("../domain/interfaces/repository");
+
+class MemoryRepository extends Repository {
+  constructor() {
+    super();
+    this.data = [];
+  }
+
+  save(item) {
+    this.data.push(item);
+    return item;
+  }
+
+  findAll() {
+    return this.data;
+  }
+}
+
+module.exports = MemoryRepository;
