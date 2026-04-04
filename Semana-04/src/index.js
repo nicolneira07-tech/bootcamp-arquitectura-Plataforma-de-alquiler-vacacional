fae9780df57176e84b9dd8aa9e90886ccf110473
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:06:43 -0500

    Update index.js

--- Semana-04/src/index.js
+++ Semana-04/src/index.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const app = express();
 
+const propertyRoutes = require('./routes/property.routes');
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./openapi.yaml');
+
+const errorHandler = require('./middleware/error-handler');
+
+app.use(express.json());
+
+// Rutas
+app.use('/api/v1/properties', propertyRoutes);
+
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Middleware de errores
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log('Servidor en http://localhost:3000');
+});
