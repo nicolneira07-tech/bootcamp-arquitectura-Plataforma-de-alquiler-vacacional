b148f27903734c087f892e10ca593f8f144da91b
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:35:04 -0500

    Update booking.service.js

--- Semana-04/src/services/booking.service.js
+++ Semana-04/src/services/booking.service.js
@@ -8,15 +8,19 @@ exports.create = (propertyId, data) => {
   const license = licenseRepo.findByProperty(propertyId);
 
   if (!license || !license.valid) {
-    throw new Error("Sin licencia válida");
+    throw new Error("Propiedad sin licencia válida");
   }
 
   const bookings = bookingRepo.findByProperty(propertyId);
   const totalDays = bookings.reduce((acc, b) => acc + b.days, 0);
 
   if (totalDays + data.days > property.maxDaysPerYear) {
-    throw new Error("Excede límite de días");
+    throw new Error("Excede el límite de días");
   }
 
   return bookingRepo.create({ propertyId, ...data });
+};
+
+exports.getByProperty = (propertyId) => {
+  return bookingRepo.findByProperty(propertyId);
 };
