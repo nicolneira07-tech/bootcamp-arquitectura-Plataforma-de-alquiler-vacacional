946813f8d97f006efa4a08533e73ec58efc223e1
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:30:34 -0500

    Update license.routes.js

--- Semana-04/src/routes/license.routes.js
+++ Semana-04/src/routes/license.routes.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/license.controller');
+
+router.post('/:id/license', controller.create);
+router.get('/:id/license', controller.getByProperty);
+
+module.exports = router;
