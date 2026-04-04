0ebf8b7a4ae025c672ba6b57ffc7459fd79b3a16
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:31:03 -0500

    Update booking.routes.js

--- Semana-04/src/routes/booking.routes.js
+++ Semana-04/src/routes/booking.routes.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/booking.controller');
+
+router.post('/:id/bookings', controller.create);
+router.get('/:id/bookings', controller.getByProperty);
+
+module.exports = router;
