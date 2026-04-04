34f7dd97b2125b2a784d887a1631bd4d86aec63a
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:33:16 -0500

    Update incident.controller.js

--- Semana-04/src/controllers/incident.controller.js
+++ Semana-04/src/controllers/incident.controller.js
@@ -1,1 +1,12 @@
 
+const service = require('../services/incident.service');
+
+exports.create = (req, res) => {
+  const data = service.create(req.params.id, req.body);
+  res.status(201).json({ success: true, data });
+};
+
+exports.getByProperty = (req, res) => {
+  const data = service.getByProperty(req.params.id);
+  res.json({ success: true, data });
+};
