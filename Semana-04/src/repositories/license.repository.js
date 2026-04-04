deee825bf53b0864786746471c6f8b69cf62479c
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:36:07 -0500

    Update license.repository.js

--- Semana-04/src/repositories/license.repository.js
+++ Semana-04/src/repositories/license.repository.js
@@ -1,1 +1,12 @@
 
+let licenses = [];
+
+exports.create = (data) => {
+  const newItem = { id: Date.now(), ...data };
+  licenses.push(newItem);
+  return newItem;
+};
+
+exports.findByProperty = (propertyId) => {
+  return licenses.find(l => l.propertyId == propertyId);
+};
