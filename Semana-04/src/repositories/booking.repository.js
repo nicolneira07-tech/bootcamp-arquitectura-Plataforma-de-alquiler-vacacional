00213d96085e4766526f7ee8249edcc4a50263b4
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:36:28 -0500

    Update booking.repository.js

--- Semana-04/src/repositories/booking.repository.js
+++ Semana-04/src/repositories/booking.repository.js
@@ -1,1 +1,12 @@
 
+let bookings = [];
+
+exports.create = (data) => {
+  const newItem = { id: Date.now(), ...data };
+  bookings.push(newItem);
+  return newItem;
+};
+
+exports.findByProperty = (propertyId) => {
+  return bookings.filter(b => b.propertyId == propertyId);
+};
