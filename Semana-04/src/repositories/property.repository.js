4308c67bffea17a88e19e24dd666c4c0442528ad
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:27:31 -0500

    Update property.repository.js

--- Semana-04/src/repositories/property.repository.js
+++ Semana-04/src/repositories/property.repository.js
@@ -1,1 +1,26 @@
 
+let properties = [];
+
+exports.findAll = (page, limit) => {
+  return properties.slice((page - 1) * limit, page * limit);
+};
+
+exports.findById = (id) => {
+  return properties.find(p => p.id == id);
+};
+
+exports.create = (data) => {
+  const newItem = { id: Date.now(), ...data };
+  properties.push(newItem);
+  return newItem;
+};
+
+exports.update = (id, data) => {
+  const index = properties.findIndex(p => p.id == id);
+  properties[index] = { ...properties[index], ...data };
+  return properties[index];
+};
+
+exports.remove = (id) => {
+  properties = properties.filter(p => p.id != id);
+};
