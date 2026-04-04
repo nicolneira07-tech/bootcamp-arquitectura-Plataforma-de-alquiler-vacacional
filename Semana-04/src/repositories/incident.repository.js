5c3861661593c14942d26862558bf646f52f3842
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:36:52 -0500

    Update incident.repository.js

--- Semana-04/src/repositories/incident.repository.js
+++ Semana-04/src/repositories/incident.repository.js
@@ -1,1 +1,12 @@
 
+let incidents = [];
+
+exports.create = (data) => {
+  const newItem = { id: Date.now(), ...data };
+  incidents.push(newItem);
+  return newItem;
+};
+
+exports.findByProperty = (propertyId) => {
+  return incidents.filter(i => i.propertyId == propertyId);
+};
