38bbc846a1814439181f8189672cffd858cb8fc6
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:26:02 -0500

    Update property.controller.js

--- Semana-04/src/controllers/property.controller.js
+++ Semana-04/src/controllers/property.controller.js
@@ -1,1 +1,35 @@
 
+const service = require('../services/property.service');
+
+exports.getAll = (req, res) => {
+  const data = service.getAll(req.query);
+  res.json({ success: true, data });
+};
+
+exports.getById = (req, res) => {
+  const data = service.getById(req.params.id);
+  if (!data) {
+    return res.status(404).json({ success: false });
+  }
+  res.json({ success: true, data });
+};
+
+exports.create = (req, res) => {
+  const data = service.create(req.body);
+  res.status(201).json({ success: true, data });
+};
+
+exports.update = (req, res) => {
+  const data = service.update(req.params.id, req.body);
+  res.json({ success: true, data });
+};
+
+exports.partialUpdate = (req, res) => {
+  const data = service.update(req.params.id, req.body);
+  res.json({ success: true, data });
+};
+
+exports.remove = (req, res) => {
+  service.remove(req.params.id);
+  res.status(204).send();
+};
