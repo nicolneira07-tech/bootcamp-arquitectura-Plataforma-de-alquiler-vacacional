b1598456362fa85d80d83296b1d5c289d9b0bf08
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:33:50 -0500

    Update license.service.js

--- Semana-04/src/services/license.service.js
+++ Semana-04/src/services/license.service.js
@@ -1,1 +1,10 @@
 
+const repo = require('../repositories/license.repository');
+
+exports.create = (propertyId, data) => {
+  return repo.create({ propertyId, ...data });
+};
+
+exports.getByProperty = (propertyId) => {
+  return repo.findByProperty(propertyId);
+};
