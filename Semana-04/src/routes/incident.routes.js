aa928c4aae05987140992a42b28d0a253571e2dd
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:31:22 -0500

    Update incident.routes.js

--- Semana-04/src/routes/incident.routes.js
+++ Semana-04/src/routes/incident.routes.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/incident.controller');
+
+router.post('/:id/incidents', controller.create);
+router.get('/:id/incidents', controller.getByProperty);
+
+module.exports = router;
