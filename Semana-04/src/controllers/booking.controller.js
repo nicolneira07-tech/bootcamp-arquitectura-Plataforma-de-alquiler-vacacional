bbf3b0ac6aa1ca3c174489c46b895f734785f9f0
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-04 18:32:26 -0500

    Update booking.controller.js

--- Semana-04/src/controllers/booking.controller.js
+++ Semana-04/src/controllers/booking.controller.js
@@ -1,1 +1,16 @@
 
+const service = require('../services/booking.service');
+
+exports.create = (req, res, next) => {
+  try {
+    const data = service.create(req.params.id, req.body);
+    res.status(201).json({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getByProperty = (req, res) => {
+  const data = service.getByProperty(req.params.id);
+  res.json({ success: true, data });
+};
