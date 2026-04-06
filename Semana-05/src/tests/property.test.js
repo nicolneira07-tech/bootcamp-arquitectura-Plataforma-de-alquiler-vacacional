a37d420265c151429ff3708aefc1ae677e476dde
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:27:02 -0500

    Update property.test.js

--- Semana-05/src/tests/property.test.js
+++ Semana-05/src/tests/property.test.js
@@ -1,1 +1,16 @@
 
+import test from "node:test";
+import assert from "node:assert";
+import { PropertyService } from "../services/propertyService.js";
+
+test("Crear propiedad", () => {
+  const service = new PropertyService();
+
+  const property = service.create({
+    type: "house",
+    title: "Test",
+    pricePerNight: 100
+  });
+
+  assert.ok(property.id);
+});
