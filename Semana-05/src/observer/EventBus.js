ceb0aed90595235a36777d6650b85c6cff94efd1
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:24:15 -0500

    Update EventBus.js

--- Semana-05/src/observer/EventBus.js
+++ Semana-05/src/observer/EventBus.js
@@ -1,1 +1,17 @@
 
+export class EventBus {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, handler) {
+    if (!this.events[event]) this.events[event] = [];
+    this.events[event].push(handler);
+  }
+
+  emit(event, data) {
+    if (this.events[event]) {
+      this.events[event].forEach(h => h(data));
+    }
+  }
+}
