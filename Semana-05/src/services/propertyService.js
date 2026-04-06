68c92e25037e481cc98207baa27431eba6264b2a
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:22:31 -0500

    Update propertyService.js

--- Semana-05/src/services/propertyService.js
+++ Semana-05/src/services/propertyService.js
@@ -1,1 +1,14 @@
 
+import { PropertyModel } from "../models/propertyModel.js";
+
+const model = new PropertyModel();
+
+export class PropertyService {
+  create(data) {
+    return model.create(data);
+  }
+
+  getAll() {
+    return model.getAll();
+  }
+}
