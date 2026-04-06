4401b8358fe3e606bd10caeed22eb7aa748699be
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:26:41 -0500

    Update Config.js

--- Semana-05/src/singleton/Config.js
+++ Semana-05/src/singleton/Config.js
@@ -1,1 +1,11 @@
 
+class Config {
+  constructor() {
+    if (Config.instance) return Config.instance;
+
+    this.appName = "Plataforma Alquiler Vacacional";
+    Config.instance = this;
+  }
+}
+
+export const config = new Config();
