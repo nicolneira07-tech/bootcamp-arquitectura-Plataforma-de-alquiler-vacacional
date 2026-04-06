6cb9a3bbf3c1f69df2d736e32b867dfe897d6753
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:22:06 -0500

    Update propertyModel.js

--- Semana-05/src/models/propertyModel.js
+++ Semana-05/src/models/propertyModel.js
@@ -1,1 +1,23 @@
 
+import { PropertyFactory } from "../patterns/factory/PropertyFactory.js";
+
+export class PropertyModel {
+  constructor() {
+    this.properties = [];
+  }
+
+  create(data) {
+    const property = PropertyFactory.create(data.type, data);
+    property.id = this.properties.length + 1;
+    this.properties.push(property);
+    return property;
+  }
+
+  getAll() {
+    return this.properties;
+  }
+
+  getById(id) {
+    return this.properties.find(p => p.id == id);
+  }
+}
