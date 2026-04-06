26a0938a9162790851a4fcae910646afa646e27e
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:23:48 -0500

    Update PricingStrategies.js

--- Semana-05/src/strategy/PricingStrategies.js
+++ Semana-05/src/strategy/PricingStrategies.js
@@ -1,1 +1,18 @@
 
+export class NormalStrategy {
+  calculate(price) {
+    return price;
+  }
+}
+
+export class DiscountStrategy {
+  calculate(price) {
+    return price * 0.9;
+  }
+}
+
+export class HighSeasonStrategy {
+  calculate(price) {
+    return price * 1.2;
+  }
+}
