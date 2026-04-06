17ea6f73eb97a0e260500cc233b043c5a22ee70d
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:22:53 -0500

    Update bookingService.js

--- Semana-05/src/services/bookingService.js
+++ Semana-05/src/services/bookingService.js
@@ -1,1 +1,25 @@
 
+import { BookingFacade } from "../patterns/facade/BookingFacade.js";
+import { PaymentAdapter } from "../patterns/adapter/PaymentAdapter.js";
+import { EventBus } from "../patterns/observer/EventBus.js";
+import { EmailObserver, LogObserver } from "../patterns/observer/Observers.js";
+import { PropertyModel } from "../models/propertyModel.js";
+import { NormalStrategy } from "../patterns/strategy/PricingStrategies.js";
+
+const eventBus = new EventBus();
+eventBus.on("bookingCreated", EmailObserver);
+eventBus.on("bookingCreated", LogObserver);
+
+const propertyModel = new PropertyModel();
+const paymentAdapter = new PaymentAdapter();
+
+const facade = new BookingFacade(propertyModel, paymentAdapter, eventBus);
+
+export class BookingService {
+  create(data) {
+    const strategy = new NormalStrategy();
+    const result = facade.createBooking(data);
+    result.booking.total = strategy.calculate(result.booking.total);
+    return result;
+  }
+}
