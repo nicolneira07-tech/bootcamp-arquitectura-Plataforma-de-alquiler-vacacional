316b30d5c8459a21eaf6f35695e1f19fa1827ba4
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:20:35 -0500

    Update propertyRoutes.js

--- Semana-05/src/ routes/propertyRoutes.js
+++ Semana-05/src/ routes/propertyRoutes.js
@@ -1,1 +1,10 @@
 
+import express from "express";
+import { createProperty, getProperties } from "../controllers/propertyController.js";
+
+const router = express.Router();
+
+router.post("/", createProperty);
+router.get("/", getProperties);
+
+export default router;
