b312095de1b1748e9897b5e47cc2b99de881836e
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:25:59 -0500

    Update PaymentAdapter.js

--- Semana-05/src/adapter/PaymentAdapter.js
+++ Semana-05/src/adapter/PaymentAdapter.js
@@ -1,1 +1,16 @@
 
+class ExternalPayment {
+  makePayment(amount) {
+    return `Pago externo: ${amount}`;
+  }
+}
+
+export class PaymentAdapter {
+  constructor() {
+    this.external = new ExternalPayment();
+  }
+
+  pay(amount) {
+    return this.external.makePayment(amount);
+  }
+}
