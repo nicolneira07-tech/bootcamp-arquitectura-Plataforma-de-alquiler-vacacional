a8949f5754dade937862d354dbc1c6d25f107920
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:26:18 -0500

    Update BookingFacade.js

--- Semana-05/src/facade/BookingFacade.js
+++ Semana-05/src/facade/BookingFacade.js
@@ -1,1 +1,23 @@
 
+export class BookingFacade {
+  constructor(propertyModel, paymentAdapter, eventBus) {
+    this.propertyModel = propertyModel;
+    this.paymentAdapter = paymentAdapter;
+    this.eventBus = eventBus;
+  }
+
+  createBooking(data) {
+    const property = this.propertyModel.getById(data.propertyId);
+
+    const payment = this.paymentAdapter.pay(property.pricePerNight);
+
+    const booking = {
+      ...data,
+      total: property.pricePerNight
+    };
+
+    this.eventBus.emit("bookingCreated", booking);
+
+    return { booking, payment };
+  }
+}
