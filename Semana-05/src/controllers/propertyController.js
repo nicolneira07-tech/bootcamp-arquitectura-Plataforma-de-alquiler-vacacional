729484ebf5ee23d42a12419d3a50f83f67fba934
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-06 12:21:20 -0500

    Update propertyController.js

--- Semana-05/src/controllers/propertyController.js
+++ Semana-05/src/controllers/propertyController.js
@@ -1,1 +1,12 @@
 
+import { PropertyService } from "../services/propertyService.js";
+
+const service = new PropertyService();
+
+export const createProperty = (req, res) => {
+  res.json(service.create(req.body));
+};
+
+export const getProperties = (req, res) => {
+  res.json(service.getAll());
+};
