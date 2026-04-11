2d0c69362a12a1bffc68f147fbfc0a8deb667afe
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-10 23:31:42 -0500

    Delete Semana-05 directory

--- Semana-05/src/controllers/bookingController.js
+++
@@ -1,8 +0,0 @@
-
-import { BookingService } from "../services/bookingService.js";
-
-const service = new BookingService();
-
-export const createBooking = (req, res) => {
-  res.json(service.create(req.body));
-};
--- Semana-05/src/observer/Observers.js
+++
@@ -1,8 +0,0 @@
-
-export const EmailObserver = (data) => {
-  console.log("📧 Email:", data);
-};
-
-export const LogObserver = (data) => {
-  console.log("📝 Log:", data);
-};
