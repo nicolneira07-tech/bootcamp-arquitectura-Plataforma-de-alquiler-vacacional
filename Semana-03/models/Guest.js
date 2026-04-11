8e7bd52a98f8be97b6f43d22f15b46661c1c61e7
nicolneira07-tech <nicolneira07@gmail.com>
2026-04-11 00:13:26 -0500

    Delete Semana-03 directory

--- Semana-03/models/Guest.js
+++
@@ -1,9 +0,0 @@
-import { Person } from "./Person.js";
-
-export class Guest extends Person {
-  #reservations = [];
-
-  addReservation(reservation) {
-    this.#reservations.push(reservation);
-  }
-}
--- Semana-03/models/Host.js
+++
@@ -1,9 +0,0 @@
-import { Person } from "./Person.js";
-
-export class Host extends Person {
-  #properties = [];
-
-  addProperty(property) {
-    this.#properties.push(property);
-  }
-}
